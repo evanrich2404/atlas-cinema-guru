26609f9ea174719155e43d510e2581d98311df6f
evanrich2404 <evanrich2404@gmail.com>
2024-07-10 16:59:34 -0500

    Fixed a bunch of the CSS and general looks of things. Mostly for the Authentication Card to look accurate

--- src/routes/auth/Login.js
+++ src/routes/auth/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './auth.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faLock, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faKey, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 const Login = ({ username, password, setUsername, setPassword }) => {
   const [showPassword, setShowPassword] = useState(false);
@@ -9,33 +9,43 @@ const Login = ({ username, password, setUsername, setPassword }) => {
   return (
     <div className='auth-form'>
       <label>Sign in with your account</label>
-      <div className='input-group'>
-        <FontAwesomeIcon icon={faUser} className='icon' />
-        <input
-          type='text'
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder='Username'
-        />
+      <div className='input-wrapper'>
+        <div className='input-header'>
+          <FontAwesomeIcon icon={faUser} className='icon' />
+          <p>Username:</p>
+        </div>
+        <div className='input-group'>
+          <input
+            type='text'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
       </div>
-      <div className='input-group show-password'>
-        <FontAwesomeIcon icon={faLock} className='icon' />
-        <input
-          type={showPassword ? 'text' : 'password'}
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder='Password'
-        />
-        <FontAwesomeIcon
-          icon={showPassword ? faEyeSlash : faEye}
-          className='toggle'
-          onMouseDown={() => setShowPassword(true)}
-          onMouseUp={() => setShowPassword(false)}
-          onMouseLeave={() => setShowPassword(false)}
-        />
+      <div className='input-wrapper'>
+        <div className='input-header'>
+          <FontAwesomeIcon icon={faKey} className='icon' />
+          <p>Password:</p>
+        </div>
+        <div className='input-group show-password'>
+          <input
+            type={showPassword ? 'text' : 'password'}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FontAwesomeIcon
+            icon={showPassword ? faEyeSlash : faEye}
+            className='toggle'
+            onMouseDown={() => setShowPassword(true)}
+            onMouseUp={() => setShowPassword(false)}
+            onMouseLeave={() => setShowPassword(false)}
+          />
+        </div>
       </div>
       <div className='submit-button'>
-        <button type='submit'>Sign In</button>
+        <button type='submit'>
+          <FontAwesomeIcon icon={faKey} className='icon' /> Sign In
+          </button>
       </div>
     </div>
   );
